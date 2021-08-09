4e381b05419a3ce2b824da2e9e9f55fcf57ea271
蔡易志 <a37029@mail.cmuh.org.tw>
2021-08-09 10:14:14 +0800

    update fn

--- drug-stock-sp/藥庫採購系統/藥庫轉採購單(1.查詢)/fn/getDrugStockInQty.sql
+++ drug-stock-sp/藥庫採購系統/藥庫轉採購單(1.查詢)/fn/getDrugStockInQty.sql
@@ -1,20 +1,20 @@
 USE [HealthResource]
 GO
 
---- 程序名稱：getDrugBranchInStockGrantQty
---- 程序說明：取得藥品全院入庫消耗量
+--- 程序名稱：getDrugStockInQty
+--- 程序說明：取得藥品全院入庫量
 --- 編訂人員：蔡易志
 --- 校閱人員：孫培然
 --- 修訂日期：2021/07/30
-CREATE FUNCTION [fn].[getDrugBranchInStockGrantQty] (@tableName VARCHAR(20), @stockNo CHAR(04), @drugCode INT, @inStockTime1 DATETIME, @inStockTime2 DATETIME)
+ALTER FUNCTION [fn].[getDrugStockInQty] (@tableName VARCHAR(20), @stockNo CHAR(04), @drugCode INT, @inStockTime1 DATETIME, @inStockTime2 DATETIME)
 RETURNS INT
 AS BEGIN
    DECLARE @grantQty INT = 0;
    DECLARE @tranType INT = 25; --交易類別 => 25: 消耗
 
-   SELECT @grantQty = ISNULL(SUM(b.Qty), 0) 
+   SELECT @grantQty = SUM(ISNULL(b.Qty, 0)) 
      FROM [dbo].[OptionItem] AS a,
-          [dbo].[DrugTrans] AS b
+          [dbo].[DrugTrans]  AS b
     WHERE a.TableName    = @tableName
       AND a.FieldName    = @stockNo
       AND b.InStockNo    = a.OptionName
--- drug-stock-sp/藥庫採購系統/藥庫轉採購單(1.查詢)/fn/getDrugStockOutQty.sql
+++ drug-stock-sp/藥庫採購系統/藥庫轉採購單(1.查詢)/fn/getDrugStockOutQty.sql
@@ -1,20 +1,20 @@
 USE [HealthResource]
 GO
 
---- 程序名稱：getDrugBranchOutStockGrantQty
---- 程序說明：取得藥品全院出庫消耗量
+--- 程序名稱：getDrugStockOutQty
+--- 程序說明：取得藥品全院出庫量
 --- 編訂人員：蔡易志
 --- 校閱人員：孫培然
 --- 修訂日期：2021/07/30
-CREATE FUNCTION [fn].[getDrugBranchOutStockGrantQty] (@tableName VARCHAR(20), @stockNo CHAR(04), @drugCode INT, @outStockTime1 DATETIME, @outStockTime2 DATETIME)
+CREATE FUNCTION [fn].[getDrugStockOutQty] (@tableName VARCHAR(20), @stockNo CHAR(04), @drugCode INT, @outStockTime1 DATETIME, @outStockTime2 DATETIME)
 RETURNS INT
 AS BEGIN
    DECLARE @grantQty INT = 0;
    DECLARE @tranType INT = 25; --交易類別 => 25: 消耗
 
-   SELECT @grantQty = ISNULL(SUM(b.Qty), 0) 
+   SELECT @grantQty = SUM(ISNULL(b.Qty, 0)) 
      FROM [dbo].[OptionItem] AS a,
-          [dbo].[DrugTrans] AS b
+          [dbo].[DrugTrans]  AS b
     WHERE a.TableName     = @tableName
       AND a.FieldName     = @stockNo
       AND b.OutStockNo    = a.OptionName
@@ -24,3 +24,4 @@ AS BEGIN
       AND b.OutStockTime <= @outStockTime2
    RETURN @grantQty
 END
+   
