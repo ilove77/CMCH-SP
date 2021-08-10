ec62a683b295d6e7a3874d85e63112944262727d
蔡易志 <a37029@mail.cmuh.org.tw>
2021-08-10 10:57:23 +0800

    commit

--- drug-stock-sp/藥庫採購系統/藥庫轉採購單(1.查詢)/fn/getDrugPurchaseQty.sql
+++ drug-stock-sp/藥庫採購系統/藥庫轉採購單(1.查詢)/fn/getDrugPurchaseQty.sql
@@ -5,14 +5,14 @@ GO
 --- 程序說明: 取得藥品採購量
 --- 編訂人員: 蔡易志
 --- 校閱人員: 孫培然
---- 修訂日期: 2021/08/06
-CREATE FUNCTION [fn].[getDrugPurchaseQty] (@supplyStock CHAR(04),@purchaseType INT, @drugCode INT, @totalGrantQty INT, @purchaseQty INT, @packageQty INT, @demandTime1 DATETIME, @demandTime2 DATETIME)
+--- 修訂日期: 2021/08/10
+CREATE FUNCTION [fn].[getDrugPurchaseQty] (@purchaseType INT, @totalQty INT, @purchaseQty INT, @packageQty INT)
 RETURNS INT
 AS BEGIN
    DECLARE @buyQty INT = 0;
 
    IF @purchaseType=5
-     SET @buyQty = CEILING(@totalGrantQty / 25) * 10 / @packageQty * @packageQty
+     SET @buyQty = CEILING(CEILING(@totalQty / 25) * 10 / @packageQty) * @packageQty
    
    IF @purchaseType=10 OR @purchaseType=11
       SET @buyQty = @purchaseQty 
