ad11fb4ce1932b8ad3b3e3d4c8a0887e19e11551
蔡易志 <a37029@mail.cmuh.org.tw>
2021-08-17 08:46:38 +0800

    update setDrugpurchaseMt

--- drug-stock-sp/藥庫入庫系統/樣品藥驗收(24.存取驗收紀錄)/setDrugPurchaseMt.sql
+++ drug-stock-sp/藥庫入庫系統/樣品藥驗收(24.存取驗收紀錄)/setDrugPurchaseMt.sql
@@ -6,17 +6,16 @@ GO
 --- 編訂人員：蔡易志
 --- 校閱人員：孫培然
 --- 修訂日期：2021/07/02
-
 CREATE PROCEDURE [dbo].[setDrugPurchaseMt](@params NVARCHAR(MAX))
 AS BEGIN
    DECLARE @systemTime    DATETIME       = GETDATE();
    DECLARE @errorSeverity INT;
-   DECLARE @purchaseNo    BIGINT         = 0;
+   DECLARE @purchaseNo    INT            = COALESCE(JSON_VALUE(@params, '$.purchaseNo'), 0);
    DECLARE @errorMessage  NVARCHAR(4000);
    DECLARE @procedureName VARCHAR(30)    = 'setDrugPurchaseMt';
 
    BEGIN TRY
-         MERGE INTO [dbo].[DrugPurchaseMt] AS a
+         MERGE INTO [dbo].[DrugPurchaseMt] AS t
          USING ( SELECT *
                    FROM OPENJSON(@params)       
                    WITH ( PurchaseNo     INT           '$.purchaseNo',
@@ -25,51 +24,67 @@ AS BEGIN
                           OrgNo          CHAR(10)      '$.orgNo',
                           SendUser       INT           '$.sendUser',
                           Remark         NVARCHAR(255) '$.remark',   
-                          SystemUser     INT           '$.systemUser'
+                          SystemUser     INT           '$.systemUser',
+                          PurchaseTime   DATETIME      '$.purchaseTime',
+                          DeliveryTime   DATETIME      '$.deliveryTime',
+                          ShipmentTime   DATETIME      '$.shipmentTime',
+                          SendTime       DATETIME      '$.sendTime'
                         )
-               ) AS b (PurchaseNo, PurchaseStatus, PurchaseUser, OrgNo, SendUser, Remark, SystemUser)
-            ON (a.PurchaseNo = b.PurchaseNo)    
-         WHEN MATCHED THEN	  
+                 ) AS s (
+                          PurchaseNo,
+                          PurchaseStatus,
+                          PurchaseUser,
+                          OrgNo,
+                          SendUser,
+                          Remark,
+                          SystemUser,
+                          PurchaseTime,
+                          DeliveryTime,
+                          ShipmentTime,
+                          SendTime
+                        )
+            ON (t.PurchaseNo = s.PurchaseNo)    
+         WHEN MATCHED THEN 
               UPDATE SET 
-                     a.PurchaseStatus = b.PurchaseStatus,
-                     a.PurchaseTime   = @systemTime,  
-                     a.PurchaseUser   = b.PurchaseUser,  
-                     a.DeliveryTime   = @systemTime,  
-                     a.OrgNo          = b.OrgNo,         
-                     a.ShipmentTime   = @systemTime,  
-                     a.SendUser       = b.SendUser,      
-                     a.SendTime       = @systemTime,      
-                     a.Remark         = b.Remark,        
-                     a.SystemUser     = b.SystemUser, 
-                     a.SystemTime     = @SystemTime   
+                     t.PurchaseStatus = ISNULL(s.PurchaseStatus,  t.PurchaseStatus),
+                     t.PurchaseTime   = ISNULL(s.purchaseTime, t.PurchaseTime), 
+                     t.PurchaseUser   = ISNULL(s.PurchaseUser, t.PurchaseUser), 
+                     t.DeliveryTime   = ISNULL(s.DeliveryTime, t.DeliveryTime), 
+                     t.OrgNo          = ISNULL(s.OrgNo, t.OrgNo),        
+                     t.ShipmentTime   = ISNULL(s.ShipmentTime, t.ShipmentTime), 
+                     t.SendUser       = ISNULL(s.SendUser, t.SendUser),    
+                     t.SendTime       = ISNULL(s.SendTime, t.SendTime),      
+                     t.Remark         = ISNULL(s.Remark, t.Remark),       
+                     t.SystemUser     = s.SystemUser, 
+                     t.SystemTime     = @systemTime   
          WHEN NOT MATCHED THEN
               INSERT (
-                      PurchaseStatus,
-                      PurchaseTime, 
-                      PurchaseUser,   
-                      DeliveryTime, 
-                      OrgNo,          
-                      ShipmentTime,   
-                      SendUser,      
-                      SendTime,       
-                      Remark, 
-                      SystemUser,    
-                      SystemTime     
+                       PurchaseStatus,
+                       PurchaseTime, 
+                       PurchaseUser,   
+                       DeliveryTime, 
+                       OrgNo,          
+                       ShipmentTime,   
+                       SendUser,      
+                       SendTime,       
+                       Remark, 
+                       SystemUser,    
+                       SystemTime     
                      )
               VALUES (
-                      b.PurchaseStatus,
-                      @systemTime, 
-                      b.PurchaseUser, 
-                      @systemTime,  
-                      b.OrgNo,         
-                      @systemTime,  
-                      b.SendUser,       
-                      @systemTime, 
-                      b.Remark, 
-                      b.SystemUser,     
-                      @systemTime 
+                       s.PurchaseStatus,
+                       s.PurchaseTime, 
+                       s.PurchaseUser, 
+                       s.DeliveryTime,  
+                       s.OrgNo,         
+                       s.ShipmentTime,  
+                       s.SendUser,       
+                       s.SendTime, 
+                       s.Remark, 
+                       s.SystemUser,     
+                       @systemTime 
                      );
-                     IF (@purchaseNo = 0) SET @purchaseNo = SCOPE_IDENTITY ();
+                     IF (@purchaseNo = 0) SET @purchaseNo = @@IDENTITY
                      
    END TRY
    BEGIN CATCH
@@ -84,13 +99,13 @@ GO
 DECLARE @params NVARCHAR(MAX) = 
 '
 {
-   "purchaseNo": 0,    
+   "purchaseNo": 15378,    
    "purchaseStatus": 70,  
    "purchaseUser": 7071,   
    "orgNo": "072",         
    "sendUser": 7068,          
    "remark": "無",        
-   "systemUser": 7068   
+   "systemUser": 37068   
 }
 ';
 
