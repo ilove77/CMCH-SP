30aee7195f8a0077788d37b64d18119b20d053fd
蔡易志 <a37029@mail.cmuh.org.tw>
2021-08-19 09:34:28 +0800

    update getDrugDeliverQty

--- drug-stock-sp/getDrugDeliverQty/getDrugDeliverQty.sql
+++ drug-stock-sp/getDrugDeliverQty/getDrugDeliverQty.sql
@@ -5,7 +5,7 @@ GO
 --- 程序說明: 取得藥品調撥(派送)量
 --- 編訂人員: 蔡易志
 --- 校閱人員: 孫培然
---- 修訂日期: 2021/08/18
+--- 修訂日期: 2021/08/19
 CREATE FUNCTION [fn].[getDrugDeliverQty] (@maxQty INT, @totalQty INT, @packageQty INT)
 RETURNS INT
 AS BEGIN
@@ -14,7 +14,7 @@ AS BEGIN
    IF @packageQty=0
       SET @packageQty = 1
 
-   SET @deliverQty = FLOOR(@maxQty - @totalQty / @packageQty) * @packageQty
+   SET @deliverQty = FLOOR((@maxQty - @totalQty) / @packageQty) * @packageQty
 
    RETURN @deliverQty
 END
