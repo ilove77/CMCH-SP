476095c647a0b8f04163dffecd8b81403e11acd8
蔡易志 <a37029@mail.cmuh.org.tw>
2021-08-19 10:01:38 +0800

    update getDrugDemandItems

--- drug-stock-sp/藥局庫存管理系統/藥局請領預估查詢/取得藥局預估撥補清單/getDrugDemandItems.sql
+++ drug-stock-sp/藥局庫存管理系統/藥局請領預估查詢/取得藥局預估撥補清單/getDrugDemandItems.sql
@@ -9,36 +9,41 @@ GO
 CREATE PROCEDURE [dbo].[getDrugDemandItems](@params NVARCHAR(MAX))
 AS BEGIN
    DECLARE @stockNo     CHAR(04) = JSON_VALUE(@params, '$.stockNo');
-   DECLARE @grantTime   DATETIME = JSON_VALUE(@params, '$.grantTime');
+   DECLARE @demandType  TINYINT  = 60; --需求類別 => 60: 自動撥補
    DECLARE @currentTime DATETIME = GETDATE();
 
-    SELECT b.MedCode    AS [medCode],
-           b.DrugName   AS [drugName],
-           a.SafetyQty  AS [safetyQty],
-           a.MaxQty     AS [maxQty],
-           a.totalQty   AS [stockQty],
-           a.PackageQty AS [packageQty],
-           [fn].[getUnitName](b.ChargeUnit)              AS [chargeUnitName],
-           [fn].[getUnitName](a.StockUnit)               AS [stockUnitName],
-           [fn].[getDrugOnWayQty](a.StockNo, a.DrugCode) AS [onWayQty],
-           [fn].[getDrugGrantQty](a.StockNo, a.DrugCode, @outStockTime1, @outStockTime2) AS [grantQty]
-      FROM DrugStockMt AS a,
-           DrugBasic   AS b
-     WHERE a.StockNo    = @stockNo
-       AND a.StartTime <= @currentTime
-       AND a.EndTime   >= @currentTime
-       AND b.DrugCode   = a.DrugCode
-       AND b.StartTime <= @currentTime
-       AND b.EndTime   >= @currentTime
+   SELECT [stockNo]        = a.StockNo,   
+          [medCode]        = b.MedCode,             
+          [drugCode]       = b.DrugCode,
+          [drugName]       = b.DrugName,
+          [safetyQty]      = a.SafetyQty, 
+          [maxQty]         = a.MaxQty, 
+          [stockQty]       = a.totalQty,
+          [packageQty]     = a.PackageQty,
+          [chargeUnitName] = [fn].[getUnitName](b.ChargeUnit),
+          [stockUnitName]  = [fn].[getUnitName](a.StockUnit),
+          [unarrivalQty]   = [fn].[getDrugOnWayQty](a.StockNo, a.DrugCode),
+          [consumeQty]     = [fn].[getDrugConsumeDemandQty](a.StockNo, a.DrugCode, @demandType) 
+     FROM [dbo].[DrugStockMt] AS a,
+          [dbo].[DrugBasic]   AS b
+    WHERE a.StockNo    = @stockNo
+      AND a.TotalQty   < a.SafetyQty
+      AND a.SafetyQty >= [fn].[getDrugOnWayQty](a.StockNo, a.DrugCode) + a.TotalQty
+      AND a.StartTime <= @currentTime
+      AND a.EndTime   >= @currentTime
+      AND b.DrugCode   = a.DrugCode
+      AND b.StartTime <= @currentTime
+      AND b.EndTime   >= @currentTime
+      AND [fn].[getDrugConsumeDemandQty](a.StockNo, a.DrugCode, 60)   <> 0  
+      AND [fn].[getDrugDeliverQty](a.MaxQty, a.TotalQty, a.PackageQty) > 0
+      FOR JSON PATH
 END
 GO
 
 DECLARE @params NVARCHAR(MAX) =
 '
 {
-   "stockNo": "1P12",
-   "outStockTime1": "2021-07-10 00:00:00.000",
-   "outStockTime2": "2021-07-13 00:00:00.000"
+   "stockNo": "1P1A"
 }
 ';
 
