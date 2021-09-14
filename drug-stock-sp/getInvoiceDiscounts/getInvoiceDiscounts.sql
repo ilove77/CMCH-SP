a0b950edd9ce9ea44f04ac893ca401dc58dbdfb9
蔡易志 <a37029@mail.cmuh.org.tw>
2021-09-14 14:41:24 +0800

    update getInvoiceDiscounts

--- drug-stock-sp/getInvoiceDiscounts/getInvoiceDiscounts.sql
+++ drug-stock-sp/getInvoiceDiscounts/getInvoiceDiscounts.sql
@@ -4,12 +4,14 @@ GO
 --- 程序說明：依發票-取得折讓資料
 --- 編訂人員：蔡易志
 --- 校閱人員：孫培然
---- 修訂日期：2021/08/09
+--- 修訂日期：2021/09/14
 CREATE PROCEDURE [dbo].[getInvoiceDiscounts](@params NVARCHAR(MAX))
 AS BEGIN
-   DECLARE @invoiceNo     CHAR(10) = JSON_VALUE(@params, '$.invoiceNo');
-   DECLARE @itemType      TINYINT  = JSON_VALUE(@params, '$.itemType');   
-   DECLARE @discountsType TINYINT  = JSON_VALUE(@params, '$.discountsType'); 
+   DECLARE @invoiceNo     CHAR(10)      = JSON_VALUE(@params, '$.invoiceNo');
+   DECLARE @itemType      TINYINT       = JSON_VALUE(@params, '$.itemType');   
+   DECLARE @discountsType TINYINT       = JSON_VALUE(@params, '$.discountsType');
+   DECLARE @itemCode      DECIMAL(12,4) = JSON_VALUE(@params, '$.itemCode');
+   DECLARE @checkNo       INT           = JSON_VALUE(@params, '$.checkNo');
 
    SELECT [discountsNo]   = a.DiscountsNo,
           [invoiceNo]     = a.InvoiceNo,
@@ -24,17 +26,20 @@ AS BEGIN
      FROM [dbo].[InvoiceDiscounts] AS a
     WHERE a.InvoiceNo     = @invoiceNo
       AND a.ItemType      = @itemType
-      AND a.DiscountsType = @discountsType
-      FOR JSON PATH
+      AND a.DiscountsType = [fn].[numberFilter] (@discountsType, a.DiscountsType)
+      AND a.ItemCode      = [fn].[numberFilter] (@itemCode, a.ItemCode)
+      AND a.CheckNo       = [fn].[numberFilter] (@checkNo, a.CheckNo)
 END
-GO;
+GO
+
 --- EXEC PROCEDURE
 DECLARE @params NVARCHAR(MAX) = 
 '
 {
-  "invoiceNo": "NW49079881",
-  "itemType" : 20,
-  "discountsType": 10
+  "invoiceNo": "CF47834118",
+  "itemType" : 10,
+  "discountsType": 10,
+  "itemCode": 2979.00
 }
 '
 EXEC [dbo].[getInvoiceDiscounts] @params
