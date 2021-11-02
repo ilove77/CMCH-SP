f568ebbbb73532d88c849d8024c13c42fe6d97d9
蔡易志 <a37029@mail.cmuh.org.tw>
2021-11-02 17:48:33 +0800

    update

---
+++ drug-stock-sp/藥庫設定檔/棟別設定/getStockBulidInfos.sql
@@ -0,0 +1,33 @@
+USE [healthCare]
+GO
+
+--- 程序名稱: getStockBulidInfos
+--- 程序說明: 取得棟別查詢資訊
+--- 編訂人員: 蔡易志
+--- 校閱人員: 孫培然
+--- 修訂日期: 2021/11/02
+CREATE PROCEDURE [dbo].[getStockBulidInfos] (@params NVARCHAR(MAX))
+AS BEGIN
+   DECLARE @departNo VARCHAR(04) = JSON_VALUE(@params,'$.departNo');
+   DECLARE @builder  CHAR(01)    = JSON_VALUE(@params,'$.builder');
+
+   SELECT [departNo]   = a.DepartNo,
+          [departName] = a.ShortName
+     FROM [dbo].[department] AS a
+    WHERE a.DepartNo LIKE @departNo 
+      AND a.Builder  = @builder
+      FOR JSON PATH
+END
+GO
+
+-- EXEC PROCEDURE
+DECLARE @params NVARCHAR(MAX) =
+'
+{
+ "departNo"  : "1%",
+ "builder" : "C"
+}
+'
+
+EXEC [dbo].[getStockBulidInfos] @params
+GO
